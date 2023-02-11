f9294f8dcb2ea43c31fcbbd147556c3d4e303a2c
Mohamed Neji Ghazouani <neji@hatchstudio.de>
2023-02-11 19:02:50 +0100

    Edit version 1.1

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,8 +11,8 @@ const Navbar = () => {
   const [showMenu, setShowMenu]=useState(false)
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
-      <div>
-        <img src={logo} alt="logo" />
+      <div >
+        <img src={logo} alt="logo"className='h-20 border border-bodyColor-100 rounded-full shadow-sm'  />
       </div>
       <div>
         <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
